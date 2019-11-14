eebaeb728ca65efea34dffe5736d7ae917b6af4f
Spenser Truex <web@spensertruex.com>
2019-11-13 22:31:01 -0800

    Formatting.

--- keybindings.el
+++ keybindings.el
@@ -1,20 +1,20 @@
 (spacemacs/declare-prefix "[" "rectangle")
 (spacemacs/set-leader-keys
-  "[c"  'close-rectangle
-  "[!"  'clear-rectangle
-  "[c"  'rectangle-number-lines
-  "[d"  'delete-rectangle
-  "[e"  'rectangle-exchange-point-and-mark
-  "[i"  'copy-rectangle-to-register
-  "[k"  'kill-rectangle
-  "[l"  'rectangle-left-char
+  "[c" 'close-rectangle
+  "[!" 'clear-rectangle
+  "[c" 'rectangle-number-lines
+  "[d" 'delete-rectangle
+  "[e" 'rectangle-exchange-point-and-mark
+  "[i" 'copy-rectangle-to-register
+  "[k" 'kill-rectangle
+  "[l" 'rectangle-left-char
   "[m" 'rectangle-mark-mode
-  "[n"  'rectangle-next-line
-  "[N"  'rectangle-previous-line
-  "[o"  'open-rectangle
-  "[p"  'rectangle-previous-line
-  "[r"  'rectangle-right-char
+  "[n" 'rectangle-next-line
+  "[N" 'rectangle-previous-line
+  "[o" 'open-rectangle
+  "[p" 'rectangle-previous-line
+  "[r" 'rectangle-right-char
   "[s" 'string-rectangle
   "[t" 'transpose-regions
-  "[v"  'rectangle-preview
-  "[y"  'yank-rectangle)
+  "[v" 'rectangle-preview
+  "[y" 'yank-rectangle)
